0489c2dd9744d432ee483202cb991ef5f5eb8ac5
Erick Almeida <ephillipe@gmail.com>
2019-09-28 19:40:54 +0100

    Merge from master

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export { HistoryActionType } from "./history-action.enum";
 export { HistoryActionColumn, HistoryEntityInterface, HistoryEntitySubscriber } from "./history-entity";
-=======
-export const Greeter = (name: string) => `Hello ${name}`; 
->>>>>>> 8b46e2a301b58b71b752dae7078ddbdc5cbfcabc
